40a938761ba03b069fc692cff2b148b73affc13c
Leo Candido Silva <lcandido@everis.com>
2020-02-21 14:14:30 -0300

    finalizacao do from lancamentos

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -16,11 +16,11 @@ export class InMemoryDatabase implements InMemoryDbService {
 
         const entries: Entry[] = [
             { id: 1, name: 'Gás de Cozinha', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "70,00", type: 'expense', description: 'qualquer descricao'} as Entry,
-            { id: 2, name: 'Suplementos', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "15,00", type: 'expense'} as Entry,
-            { id: 3, name: 'Salário na Empresa X', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "4400,00", type: 'revenue', description: 'qualquer descricao'} as Entry,
-            { id: 4, name: 'Aluguel de Filme', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "15,00", type: 'expense'} as Entry,
-            { id: 5, name: 'Suplementos', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "30,00", type: 'expense'} as Entry,
-            { id: 6, name: 'Vídeo Game da Filha', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "15,00", type: 'expense'} as Entry,
+            { id: 2, name: 'Suplementos', categoryId: categories[1].id, category: categories[1], paid: true, date: '14/10/2018', amount: "15,00", type: 'expense'} as Entry,
+            { id: 3, name: 'Salário na Empresa X', categoryId: categories[3].id, category: categories[3], paid: true, date: '14/10/2018', amount: "4400,00", type: 'revenue', description: 'qualquer descricao'} as Entry,
+            { id: 4, name: 'Aluguel de Filme', categoryId: categories[2].id, category: categories[2], paid: true, date: '14/10/2018', amount: "15,00", type: 'expense'} as Entry,
+            { id: 5, name: 'Suplementos', categoryId: categories[1].id, category: categories[1], paid: true, date: '14/10/2018', amount: "30,00", type: 'expense'} as Entry,
+            { id: 6, name: 'Vídeo Game da Filha', categoryId: categories[2].id, category: categories[2], paid: true, date: '14/10/2018', amount: "15,00", type: 'expense'} as Entry,
             { id: 7, name: 'Uber', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "30,00", type: 'expense'} as Entry,
             { id: 8, name: 'Aluguel', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "15,00", type: 'expense'} as Entry,
             { id: 9, name: 'Gás de Cozinha', categoryId: categories[0].id, category: categories[0], paid: true, date: '14/10/2018', amount: "30,00", type: 'expense', description: 'qualquer descricao'} as Entry,
--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -3,15 +3,15 @@ import { Category } from '../../categories/shared/category.model';
 export class Entry {
     
     constructor(
-        public id: number,
-        public name: string,
-        public description: string,
-        public type: string,
-        public amount: string,
-        public date: string,
-        public paid: boolean,
-        public categoryId: number,
-        public category: Category
+        public id: number = null,
+        public name: string = null,
+        public description: string = null,
+        public type: string = null,
+        public amount: string = null,
+        public date: string = null,
+        public paid: boolean = false,
+        public categoryId: number = null,
+        public category: Category = null
     ) { }
 
     static types = {
